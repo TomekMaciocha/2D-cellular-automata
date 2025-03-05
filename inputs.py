f69e96dfd2aca58c2c9867dee0ed5f3e1da75556
TomekMaciocha <tmaciocha95@gmail.com>
2025-03-05 15:20:29 +0100

    Moved input methods to new 'Inputs' class in inputs.py

---
+++ inputs.py
@@ -0,0 +1,130 @@
+
+class Inputs:
+
+    def sim_size() -> int:
+        """This method is used to manually enter the size of the simulated system."""
+        print("Set size of the simulation grid (3 to 1000, default: 100):")
+        while True:
+            tmp = input()
+            if tmp == "":
+                return 100
+            elif int(tmp) < 3 or int(tmp) > 1000:
+                print("Input not in specified range.")
+                continue
+            return int(tmp)
+
+
+    def sim_probability() -> float:
+        """This method is used to manually enter the initial probability of cells being alive in the simulated system."""
+        print(
+            "Set probability of a cell being alive on the starting iteration (default: 0.5):"
+        )
+        while True:
+            tmp = input()
+            if tmp == "":
+                return 0.5
+            elif float(tmp) < 0 or float(tmp) > 1:
+                print("Probability has to be between 0 and 1")
+                continue
+            return float(tmp)
+
+
+    def sim_rules() -> list[list[int]]:
+        """This method is used to manually enter the cellular automaton rules applying to the simulated system."""
+        print(
+            "Input numbers of alive neighbors required for a dead cell to become alive (0 to 8, none to end this setting):"
+        )
+        rules = [[], []]
+        tmp = 1
+        while True:
+            tmp = input()
+            if tmp == "":
+                break
+            elif int(tmp) < 0 or int(tmp) > 8:
+                print("Input not in specified range.")
+                continue
+            elif int(tmp) in rules[0]:
+                print("This rule is already in place.")
+                continue
+            rules[0].append(int(tmp))
+
+        print(
+            "Input numbers of alive neighbors required for an alive cell to stay alive (0 to 8, none to end this setting):"
+        )
+
+        tmp = 1
+        while True:
+            tmp = input()
+            if tmp == "":
+                break
+            elif int(tmp) < 0 or int(tmp) > 8:
+                print("Input not in specified range.")
+                continue
+            elif int(tmp) in rules[1]:
+                print("This rule is already in place.")
+                continue
+            rules[1].append(int(tmp))
+        return rules
+
+
+    def sim_n_steps() -> int:
+        """This method is used to manually enter the length of the simulation to be performed."""
+        tmp = input("input number of steps in the simulation (default: 100):\n")
+        if tmp == "":
+            return 100
+        else:
+            return int(tmp)
+
+
+    def vis_figsize() -> float:
+        """This method is used to manually enter the size of the figure used for generating animation frames."""
+        print("Set size of the square figure used for generating frames (default: 10):")
+        while True:
+            tmp = input()
+            if tmp == "":
+                return 10
+            elif float(tmp) <= 0:
+                print("The size must be a positive number.")
+                continue
+            return float(tmp)
+
+
+    def vis_fps() -> float:
+        """This method is used to manually enter the number of frames per second in the animation to be generated."""
+        print("Set number of frames per second in the animation (0.2 to 300, default: 5):")
+        while True:
+            tmp = input()
+            if tmp == "":
+                return 5
+            elif float(tmp) < 0.2 or float(tmp) > 300:
+                print("Input not in specified range.")
+                continue
+            return float(tmp)
+
+
+    def vis_n_frames(n_steps) -> int:
+        """This method is used to manually enter the number of frames in the animation to be generated."""
+        print(
+            "Set number of frames in the animation (1 to "
+            + str(n_steps)
+            + ", default: "
+            + str(n_steps)
+            + "):"
+        )
+        while True:
+            tmp = input()
+            if tmp == "":
+                return n_steps
+            elif int(tmp) < 1 or int(tmp) > n_steps:
+                print("Input not in specified range.")
+                continue
+            return int(tmp)
+
+
+    def vis_animation_name() -> str:
+        """This method is used to set the name of a file in which the generated animation will be saved"""
+        print("Set the filename of the animation without extension (default: 'ani'):")
+        tmp = input()
+        if tmp == "":
+            tmp = "ani"
+        return tmp
--- main.py
+++ main.py
@@ -2,139 +2,10 @@ import os
 import glob
 from simulation import Simulation
 from visualization import Visualization
+from inputs import Inputs
 
 OUTPUT_FOLDER = "output_files"
 VISUALISATION_FOLDER = "visualizations"
-
-
-def sim_set_size() -> int:
-    """This function is used to manually enter the size of the simulated system."""
-    print("Set size of the simulation grid (3 to 1000, default: 100):")
-    while True:
-        tmp = input()
-        if tmp == "":
-            return 100
-        elif int(tmp) < 3 or int(tmp) > 1000:
-            print("Input not in specified range.")
-            continue
-        return int(tmp)
-
-
-def sim_set_probability() -> float:
-    """This function is used to manually enter the initial probability of cells being alive in the simulated system."""
-    print(
-        "Set probability of a cell being alive on the starting iteration (default: 0.5):"
-    )
-    while True:
-        tmp = input()
-        if tmp == "":
-            return 0.5
-        elif float(tmp) < 0 or float(tmp) > 1:
-            print("Probability has to be between 0 and 1")
-            continue
-        return float(tmp)
-
-
-def sim_set_rules() -> list[list[int]]:
-    """This function is used to manually enter the cellular automaton rules applying to the simulated system."""
-    print(
-        "Input numbers of alive neighbors required for a dead cell to become alive (0 to 8, none to end this setting):"
-    )
-    rules = [[], []]
-    tmp = 1
-    while True:
-        tmp = input()
-        if tmp == "":
-            break
-        elif int(tmp) < 0 or int(tmp) > 8:
-            print("Input not in specified range.")
-            continue
-        elif int(tmp) in rules[0]:
-            print("This rule is already in place.")
-            continue
-        rules[0].append(int(tmp))
-
-    print(
-        "Input numbers of alive neighbors required for an alive cell to stay alive (0 to 8, none to end this setting):"
-    )
-
-    tmp = 1
-    while True:
-        tmp = input()
-        if tmp == "":
-            break
-        elif int(tmp) < 0 or int(tmp) > 8:
-            print("Input not in specified range.")
-            continue
-        elif int(tmp) in rules[1]:
-            print("This rule is already in place.")
-            continue
-        rules[1].append(int(tmp))
-    return rules
-
-
-def sim_set_n_steps() -> int:
-    """This function is used to manually enter the length of the simulation to be performed."""
-    tmp = input("input number of steps in the simulation (default: 100):\n")
-    if tmp == "":
-        return 100
-    else:
-        return int(tmp)
-
-
-def vis_set_figsize() -> float:
-    """This function is used to manually enter the size of the figure used for generating animation frames."""
-    print("Set size of the square figure used for generating frames (default: 10):")
-    while True:
-        tmp = input()
-        if tmp == "":
-            return 10
-        elif float(tmp) <= 0:
-            print("The size must be a positive number.")
-            continue
-        return float(tmp)
-
-
-def vis_set_fps() -> float:
-    """This function is used to manually enter the number of frames per second in the animation to be generated."""
-    print("Set number of frames per second in the animation (0.2 to 300, default: 5):")
-    while True:
-        tmp = input()
-        if tmp == "":
-            return 5
-        elif float(tmp) < 0.2 or float(tmp) > 300:
-            print("Input not in specified range.")
-            continue
-        return float(tmp)
-
-
-def vis_set_n_frames(n_steps) -> int:
-    """This function is used to manually enter the number of frames in the animation to be generated."""
-    print(
-        "Set number of frames in the animation (1 to "
-        + str(n_steps)
-        + ", default: "
-        + str(n_steps)
-        + "):"
-    )
-    while True:
-        tmp = input()
-        if tmp == "":
-            return n_steps
-        elif int(tmp) < 1 or int(tmp) > n_steps:
-            print("Input not in specified range.")
-            continue
-        return int(tmp)
-
-
-def vis_set_animation_name() -> str:
-    """This function is used to set the name of a file in which the generated animation will be saved"""
-    print("Set the filename of the animation without extension (default: 'ani'):")
-    tmp = input()
-    if tmp == "":
-        tmp = "ani"
-    return tmp
-
 
 def main() -> None:
     print(
@@ -153,15 +24,15 @@ def main() -> None:
 
         if sim_settings == "2":
             sim = Simulation(
-                sim_set_size(),
-                sim_set_probability(),
-                sim_set_rules(),
-                sim_set_n_steps(),
+                Inputs.sim_size(),
+                Inputs.sim_probability(),
+                Inputs.sim_rules(),
+                Inputs.sim_n_steps(),
                 OUTPUT_FOLDER,
             )
         elif sim_settings == "1":
             sim = Simulation(
-                100, 0.5, sim_set_rules(), sim_set_n_steps(), OUTPUT_FOLDER
+                100, 0.5, Inputs.sim_rules(), Inputs.sim_n_steps(), OUTPUT_FOLDER
             )
         else:
             sim = Simulation(100, 0.5, [[3], [3, 2]], 100, OUTPUT_FOLDER)
@@ -175,10 +46,10 @@ def main() -> None:
 
             if vis_settings == "Y" or vis_settings == "y":
                 vis = Visualization(
-                    vis_set_figsize(),
-                    vis_set_fps(),
-                    vis_set_n_frames(sim.n_steps),
-                    vis_set_animation_name(),
+                    Inputs.vis_figsize(),
+                    Inputs.vis_fps(),
+                    Inputs.vis_n_frames(sim.n_steps),
+                    Inputs.vis_animation_name(),
                     OUTPUT_FOLDER,
                     VISUALISATION_FOLDER,
                 )
@@ -188,7 +59,7 @@ def main() -> None:
                     10,
                     5,
                     sim.n_steps,
-                    vis_set_animation_name(),
+                    Inputs.vis_animation_name(),
                     OUTPUT_FOLDER,
                     VISUALISATION_FOLDER,
                 )
